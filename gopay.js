2078613125153ddcd13327e71fdc018570a90279
codexyan <mdcodeid@gmail.com>
2023-08-07 08:24:29 +0700

    Fix based on review

--- gopay.js
+++ gopay.js
@@ -1,11 +1,11 @@
-// Ini fitur gopay dengan fee 0%
+// Ini fitur gopay dengan fee 0% sudah termasuk PPN
 class Gopay {
     constructor(idr, number){
         this.idr = idr;
         this.number = number;
     }
 
-    // Fungsi notification
+    // Fungsi notification with framework JS
     getNotification(name){
         alert(`Halo kak ${name}, saldo Anda ${this.number} ${this.idr}`)
     }
